33f13a46ebc72924f02d9d1a5a069a26b61d1e4c
JericoL <Jericolabajo11@Gmail.com>
2025-04-12 14:07:33 +0800

    Dashboard Setup Lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'dashboard', pathMatch: 'full'
+    },
+
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./Dashboard/dashboard.module').then(m => m.DashboardModule)
+    }
+];
